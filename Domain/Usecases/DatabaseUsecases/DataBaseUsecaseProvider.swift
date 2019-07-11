deef77690b8288479f3ce6648bb45bfa3d4f97dc
behrad kazemi <behradkzm@gmail.com>
2019-07-11 20:53:19 +0430

    add QueryManager to dataBase usecase

--- Domain/Usecases/DatabaseUsecases/DataBaseUsecaseProvider.swift
+++ Domain/Usecases/DatabaseUsecases/DataBaseUsecaseProvider.swift
@@ -10,5 +10,6 @@ import Foundation
 import RxSwift
 
 public protocol DataBaseUsecaseProvider {
+	func makeQueryManager() -> QueryManager
 	func makePlayStageUseCase(suggestion: SuggestionUsecase) -> PlayStageUsecase
 }
